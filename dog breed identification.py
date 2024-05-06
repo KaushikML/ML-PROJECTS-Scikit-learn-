374f9732f95b17ebbaec451c3fe6d24faca443a0
Kaushik Roy <roykaushik354@gmail.com>
2024-05-06 18:06:05 +0530

    Update and rename dog breed identification.txt to dog breed identification.py

--- dog breed identification.py
+++ dog breed identification.py
@@ -1,4 +1,4 @@
-1. Problem
+#/1. Problem
 Identifying the breed of a dog given an image of a dog.
 
 When I'm sitting at the cafe and I take a photo of a dog, I want to know what breed of dog it is.
